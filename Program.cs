13055615b169e5612858d045f67183bb6ff14cdb
Mark McArthey <mcarthey@gmail.com>
2021-09-29 19:50:28 -0500

    Initial program structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WCTC_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
